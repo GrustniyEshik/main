89eb8ffc217957805c2155aa2d6df071881f44a4
Sergey Malakhov <28032005grust@gmail.com>
2024-01-26 17:55:15 +0300

    3

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function go(id) {
+    
     location.href = "D:\\Сайт_для_OST\\"+id+"\\"+id+".html";
 }
 
 function load(id) {
     block = document.getElementById("blockPopularGoods"+id[id.length-1]);
-    block.innerHTML = `<div class="Goods" id="goods2.1"></div> <div class="Goods" id="goods2.2"></div>`
+    block.innerHTML = ` <div class="Goods" id="goods1.1"></div>
+                        <div class="Goods" id="goods1.2"></div>`
 }
