ec7f047590d0e7240b9cd9963c3c2c483ce9a3c7
Sergey Malakhov <28032005grust@gmail.com>
2024-01-26 17:49:40 +0300

    3

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function go(id) {
 
 function load(id) {
     block = document.getElementById("blockPopularGoods"+id[id.length-1]);
-    block.innerHTML = "КУ"
+    block.innerHTML = "<div class="Goods" id="goods2.1"></div> <div class="Goods" id="goods2.2"></div>"
+    
 }
