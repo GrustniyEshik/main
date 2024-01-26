a7a1631616e3cde120fe90bfe0e89f40276157a4
Sergey Malakhov <28032005grust@gmail.com>
2024-01-26 17:50:49 +0300

    Изменение  "" на ``

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ function go(id) {
 
 function load(id) {
     block = document.getElementById("blockPopularGoods"+id[id.length-1]);
-    block.innerHTML = "<div class="Goods" id="goods2.1"></div> <div class="Goods" id="goods2.2"></div>"
-    
+    block.innerHTML = `<div class="Goods" id="goods2.1"></div> <div class="Goods" id="goods2.2"></div>`
 }
