f8b1fb378bb637f0c4ddbe14b5bd4dac0c84deae
Sergey Malakhov <28032005grust@gmail.com>
2024-02-02 17:13:04 +0300

    3

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ function go(id) {
 }
 
 function load(id) {
-    block = document.getElementById("blockPopularGoods"+id[id.length-1]);
+    block = document.getElementById("blockPopularGoods2");
+
+    direction = document.getElementById("popularGoods"+id[id.length-1]);
+
     block.innerHTML = ` <div class="Goods" id="goods1.1"></div>
+                        <div class="Goods" id="goods1.2"></div>
+                        <div class="Goods" id="goods1.2"></div>
+                        <div class="Goods" id="goods1.2"></div>
                         <div class="Goods" id="goods1.2"></div>`
 }
