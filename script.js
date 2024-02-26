e94b4099e141416dd16f4c4258a97916964fae9b
Sergey Malakhov <28032005grust@gmail.com>
2024-02-26 09:45:25 +0300

    Переделывание CSS

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
 function go(id) {
-    
     location.href = "D:\\Сайт_для_OST\\"+id+"\\"+id+".html";
 }
 
 function load(id) {
-    block = document.getElementById("blockPopularGoods2");
+    x = document.getElementById("scrollPopularGoods2");
 
-    direction = document.getElementById("popularGoods"+id[id.length-1]);
+    x.innerHTML = 
+    `<div class="Goods" id="goods1.1"></div>
+    <div class="Goods" id="goods1.2"></div>
+    <div class="Goods" id="goods1.3"></div>
+    <div class="Goods" id="goods1.4"></div>
+    <div class="Goods" id="goods1.5"></div>`
+}
 
-    block.innerHTML = ` <div class="Goods" id="goods1.1"></div>
-                        <div class="Goods" id="goods1.2"></div>
-                        <div class="Goods" id="goods1.2"></div>
-                        <div class="Goods" id="goods1.2"></div>
-                        <div class="Goods" id="goods1.2"></div>`
+x = 0;
+function theme() {
+    if (x == 0) {
+        dark_theme();
+        x = 1;
+    } else {
+        light_theme();
+        x = 0;
+    }
+
+}
+function dark_theme() {
+    logo1 = document.getElementById("logoImg1");
+    logo2 = document.getElementById("logoImg2");
+    logo1.src = "IMG/LogoWhite.png";
+    logo2.src = "IMG/LogoWhite.png";
+
+    document.head.innerHTML =
+    `<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="desktop.css">
+    <link rel="stylesheet" href="mobile.css">
+    <link rel="stylesheet" href="dark.css">
+    <link rel="stylesheet" href="FONTS/fonts.css">`;
+}
+function light_theme() {
+    logo1 = document.getElementById("logoImg1");
+    logo2 = document.getElementById("logoImg2");
+    logo1.src = "IMG/LogoBlack.png";
+    logo2.src = "IMG/LogoBlack.png";
+
+    document.head.innerHTML =
+    `<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="desktop.css">
+    <link rel="stylesheet" href="mobile.css">
+    <link rel="stylesheet" href="FONTS/fonts.css">`;
 }
