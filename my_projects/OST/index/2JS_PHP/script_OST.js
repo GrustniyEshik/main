2da4d22178de9c621a7cbf876c8053d2558f9930
Sergey Malakhov <28032005grust@gmail.com>
2024-05-29 17:28:46 +0300

    F

--- my_projects/OST/index/2JS_PHP/script_OST.js
+++ my_projects/OST/index/2JS_PHP/script_OST.js
@@ -1,5 +1,5 @@
 function go(id) {
-    location.href = `https://grustniyeshik.github.io/OST/`+id+`/`+id+`.html`;
+    location.href = `https://grustniyeshik.github.io/main/my_projects/OST/`+id+`/`+id+`.html`;
 }
 
 
