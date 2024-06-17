9ea298881c738ed62c62d420edaa2131f8a36512
Sergey Malakhov <28032005grust@gmail.com>
2024-06-18 01:01:31 +0300

    AAAAA

--- my_projects/OST/goods/2JS_PHP/goods.js
+++ my_projects/OST/goods/2JS_PHP/goods.js
@@ -7,9 +7,15 @@ function start () {
     //Через цикл добавляем в окно кнопки с изображением товаров
     for (let i = 1; i <= 10; i++) {
         x.innerHTML = x.innerHTML + 
-        `<button class="Goods" id="goods${i}"><img src="3IMG/Goods/Good_${i}.png">
-        </button>`
-        console.log (i);
+        `<button class="Goods" id="goods${i}"><img src="3IMG/Goods/Good_${i}.png"></button>`
+    }
+
+
+    allGoods = document.getElementById("blockAllGoods");
+    allGoods.innerHTML = "";
+    for (let i = 1; i <= 10; i++) {
+        allGoods.innerHTML = allGoods.innerHTML + 
+        `<button class="Goods" id="goods${i}"><img src="3IMG/Goods/Good_${i}.png"></button>`
     }
 }
 
