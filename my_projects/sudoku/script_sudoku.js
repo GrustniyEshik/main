9b31544fae149c4672a5ac354f15a134c2ced8c5
Гыук <28032005grust@gmail.com>
2025-11-10 19:40:29 +0300

    10.11.2025

--- my_projects/sudoku/script_sudoku.js
+++ my_projects/sudoku/script_sudoku.js
@@ -1,1 +1,21 @@
-array_cells=document.querySelectorAll(".subcells"); 
+array_cells=document.querySelectorAll(".cell");
+array_numbers=document.querySelectorAll(".number");
+
+for (let i=0; i<array_cells.length; i++){                                               //Цикл i от 0 до array_cells.length (9)
+    array_cells[i].addEventListener("click", click_on_cell);                            //Добавление событие "Нажатия" каждому элементу массива с ячейками и указание названия функции выполняющейся при нажатии
+}
+
+for (let i=0; i<array_numbers.length; i++){                                               //Цикл i от 0 до array_cells.length (9)
+    array_numbers[i].addEventListener("click", select_nomber);                            //Добавление событие "Нажатия" каждому элементу массива с ячейками и указание названия функции выполняющейся при нажатии
+}
+
+function select_nomber() {
+    number=this;
+    console.log("Выдрана цифра", number.id);
+}
+
+function click_on_cell() {
+    cell=this;
+    cell.innerText=number.innerText;
+    console.log("Вы поставили ", number.innerText, " в ячейку ",cell.id);
+}
---
+++ my_projects/sudoku/script_test.js
@@ -0,0 +1,36 @@
+function chek() {
+    for (let i=0; i<array_cells.length; i++) {
+        console.log("В ячейке ", array_cells[i].id, " стоит ", array_cells[i].innerText);
+    }
+}
+function test(x) {
+    if (x==1) {
+        array_cells[0].innerText=6;
+        array_cells[1].innerText=8;
+        array_cells[2].innerText=5;
+        array_cells[3].innerText=1;
+        array_cells[4].innerText=3;
+        array_cells[5].innerText=2;
+        array_cells[6].innerText=4;
+        array_cells[7].innerText=7;
+        array_cells[8].innerText=9;
+        array_cells[9].innerText=7;
+        array_cells[10].innerText=3;
+        array_cells[11].innerText=4;
+        array_cells[12].innerText=5;
+        array_cells[13].innerText=9;
+        array_cells[14].innerText=8;
+        array_cells[15].innerText=1;
+        array_cells[16].innerText=6;
+        array_cells[17].innerText=2;
+        array_cells[18].innerText=2;
+        array_cells[19].innerText=1;
+        array_cells[20].innerText=9;
+        array_cells[21].innerText=7;
+        array_cells[22].innerText=6;
+        array_cells[23].innerText=4;
+        array_cells[24].innerText=5;
+        array_cells[25].innerText=3;
+        array_cells[26].innerText=8;
+    }
+}
